72930108c3403af7af8f1ae66f2c8308d4b4a013
Artyom Dubtsov <artd200258@gmail.com>
2025-11-19 02:24:51 +0300

    654654

--- crates/bisection/src/lib.rs
+++ crates/bisection/src/lib.rs
@@ -9,17 +9,17 @@ pub fn bisect<F>(
 where
     F: Fn(f64) -> f64,
 {
-    if func(left_limit).signum() == func(right_limit).signum() {
-        return Err(Errors::IncorrectRange);
-    }
     if func(left_limit) == 0.0 {
         return Ok(left_limit);
-    } 
+    }
     if func(right_limit) == 0.0 {
         return Ok(right_limit);
     }
+    if func(left_limit).signum() == func(right_limit).signum() {
+        return Err(Errors::IncorrectRange);
+    }
     let mut mid = 0.0;
-    while (right_limit - left_limit).abs() > precision {
+    while (right_limit - left_limit).abs() >= precision {
         mid = left_limit + (right_limit - left_limit) / 2.0;
         if func(left_limit).signum() != func(mid).signum() {
             right_limit = mid;
@@ -34,11 +34,11 @@ where
 mod tests {
     use super::*;
 
-    let 
     #[test]
-    fn it_works() {
-        let result = bisect(|x| 5.0 * x - 20.0, 4.0, 5.0, 0.005);
+    fn test_precison() {
+        let result = bisect(|x| 5.0 * x - 20.0, 3.0, 5.0, 0.005);
         assert!(result.is_ok());
+        assert!(result.unwrap() - 4.0 < 0.005);
     }
 
     #[test]
@@ -46,5 +46,12 @@ mod tests {
         let result = bisect(|x| 5.0 * x - 20.0, 5.0, 6.0, 0.005);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), Errors::IncorrectRange);
+    }
+
+    #[test]
+    fn it_works() {
+        let result = bisect(|x| 5.0 * x - 20.0, 4.0, 5.0, 0.005);
+        //assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 4.0);
     }
 }
